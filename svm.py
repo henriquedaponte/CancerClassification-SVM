519f3c10a2d3f35c9919fcd49a46ce7eb6db2630
henriquedaponte <henriquedaponte@gmail.com>
2023-11-20 12:31:59 -0800

    added more specific and descriptive inline comments

--- svm.py
+++ svm.py
@@ -34,21 +34,20 @@ def loadData(filename):
     - y_test (np.array): Labels for the test data.
     """
 
+    # Load the dataset, handling missing values with KNN imputation, and split into training and testing sets.
     data = pd.read_csv(filename, delimiter=',', header=None, na_values='?')
 
-    # Initialize the KNNImputer
-    imputer = KNNImputer(n_neighbors=5)  
-
-    # Perform the imputation
+    # Initialize the imputer and apply it to fill in missing values based on neighboring samples.
+    imputer = KNNImputer(n_neighbors=5)
     data_imputed = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
 
-    # Using 70% of the data for training
+    # Determine the size for training data (70% of the total dataset)
     trainDataSize = int(0.7 * data_imputed.shape[0])
+
+    # Split the imputed data into training and testing sets
     trainData = data_imputed.iloc[:trainDataSize, :]
     X_train = trainData.iloc[:, 1:-1].values # Ignoring the first column (id)
     y_train = trainData.iloc[:, -1].values
-
-    # Using 30% of the data for testing
     testData = data_imputed.iloc[trainDataSize:, :]
     X_test = testData.iloc[:, 1:-1].values # Ignoring the first column (id)
     y_test = testData.iloc[:, -1].values
@@ -57,10 +56,10 @@ def loadData(filename):
     y_train = (np.where(y_train == 2, -1, 1)).reshape(-1, 1)
     y_test = np.where(y_test == 2, -1, 1).reshape(-1, 1)
 
-    # Initialize the scaler
+    # Normalize the features to have zero mean and unit variance, as this often improves the performance of SVMs.
     scaler = StandardScaler()
 
-    # Normalizing the data
+    # Fit the scaler on the training data and transform both training and test data.
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
@@ -80,12 +79,15 @@ def svmTrain(X_train, y_train, gamma):
     - b (cp.Variable): The learned bias term.
     """
 
-    # Defining the decsion variables
+    # Define the decision variables for SVM's linear weights and bias term.
     a = cp.Variable((X_train.shape[1], 1))
     b = cp.Variable()
+
+    # Slack variable eta for handling non-linearly separable data
     eta = cp.Variable((X_train.shape[0], 1))
 
     # Defining the objective function
+    # The objective function is composed of the norm of 'a' and the L1 norm of slack variables, weighted by gamma.
     objective = cp.Minimize(cp.norm2(a) + gamma * cp.norm1(eta))
 
     # Defining the constraints
@@ -94,7 +96,7 @@ def svmTrain(X_train, y_train, gamma):
     # Defining the problem
     problem = cp.Problem(objective, constraints)
 
-    # Solving the problem
+    # Solve the optimization problem and return the learned parameters.
     problem.solve()
 
     return a.value, b.value
@@ -113,10 +115,11 @@ def svmTest(a, b, X_test, y_test):
     - loss (float): The calculated 0-1 loss.
     """
 
-    # Calculating the predicted values
+   # Predict labels for the test set and compare with the true labels to compute the loss.
     y_pred = np.sign(np.dot(X_test, a) - b)
 
     # Calculating 0-1 loss
+    # 0-1 loss is calculated as the fraction of misclassified examples.
     loss = 1 - np.sum(y_pred == y_test) / y_test.shape[0]
 
     return loss
@@ -131,9 +134,11 @@ def plotData(train_errors, test_errors, gamma):
     - gamma (list): List of gamma values used in training.
     """
 
+    # Convert errors to percentages for easier interpretation.
     train_errors = np.array(train_errors) * 100
     test_errors = np.array(test_errors) * 100
 
+    # Plot the training and test errors against the gamma values.
     plt.plot(gamma, train_errors, label='Train Error')
     plt.plot(gamma, test_errors, label='Test Error')
     plt.xlabel('Gamma')
